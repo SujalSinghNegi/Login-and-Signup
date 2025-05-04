73eeb5a2c5687de56ebec859314404cd82061846
Sujal Singh Negi <negisujal2007@gmail.com>
2025-05-04 16:44:22 +0530

    layout adjusted

--- app/src/main/java/com/example/loginandsignup/LoginActivity.kt
+++ app/src/main/java/com/example/loginandsignup/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.style.ForegroundColorSpan
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,7 +15,18 @@ import com.example.loginandsignup.databinding.ActivityLoginBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import android.widget.Toast
+import androidx.credentials.ClearCredentialStateRequest
+import androidx.credentials.Credential
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.exceptions.ClearCredentialException
+import androidx.lifecycle.lifecycleScope
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential.Companion.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.coroutines.launch
 
 import kotlin.getValue
 
@@ -67,9 +79,67 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
-        binding.google.setOnClickListener {
 
+
+
+
+
+        binding.google.setOnClickListener {
+// Instantiate a Google sign-in request
+            val googleIdOption = GetGoogleIdOption.Builder()
+                // Your server's client ID, not your Android client ID.
+                .setServerClientId(getString(R.string.default_web_client_id))
+                // Only show accounts previously used to sign in.
+                .setFilterByAuthorizedAccounts(true)
+                .build()
+
+// Create the Credential Manager request
+            val request = GetCredentialRequest.Builder()
+                .addCredentialOption(googleIdOption)
+                .build()
         }
     }
+    private fun handleSignIn(credential: Credential) {
+        // Check if credential is of type Google ID
+        if (credential is CustomCredential && credential.type == TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+            // Create Google ID Token
+            val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(credential.data)
 
+            // Sign in to Firebase with using the token
+            firebaseAuthWithGoogle(googleIdTokenCredential.idToken)
+        } else {
+            Log.w("TAG", "Credential is not of type Google ID!")
+        }
+    }
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "signInWithCredential:success")
+                    val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user
+                    Log.w("TAG", "signInWithCredential:failure", task.exception)
+                   // updateUI(null)
+                }
+            }
+    }
+    private fun signOut() {
+        // Firebase sign out
+        auth.signOut()
+
+        // When a user signs out, clear the current user credential state from all credential providers.
+        lifecycleScope.launch {
+            try {
+                val clearRequest = ClearCredentialStateRequest()
+            //    credentialManager.clearCredentialState(clearRequest)
+             //   updateUI(null)
+            } catch (e: ClearCredentialException) {
+                Log.e("TAG", "Couldn't clear user credentials: ${e.localizedMessage}")
+            }
+        }
+    }
 }
